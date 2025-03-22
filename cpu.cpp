2a63ba60637bdb67cf61198ca56fbb9150f237cb
LukaUE031 <163162773+LukaUE031@users.noreply.github.com>
2025-03-22 20:53:44 +0100

    Funkcija game_Play za cpu.cpp
    Dodata game_Play funkcija za igranje kada potez prvi igra racunar ili igrac, treba dodati ime funkcije u cpu.hpp file

--- cpu.cpp
+++ cpu.cpp
@@ -1,3 +1,37 @@
+/* game_Play u kojoj irac ima prvi potez (u ovom slucaju potezi zadati datotekom input.txt)
+int CPU::game_play(){
+srand(time(0));
+int tempAI;
+
+while(true){
+
+
+PlayPostion('X'); //Korisnik bira potez
+Board();
+tempAI=AIManager();
+
+uint8_t Reg_Value=get_ip();
+
+if(Reg_Value!=0){
+		
+		if(Reg_Value==1){
+		cout<<endl<<"Player 2 WON!";
+			return 0;}
+		else if(Reg_Value==2){
+		cout<<endl<<"Player 1 WON!";
+		return 0;}
+		else if(Reg_Value==3){
+		cout<<"You Tie !"
+		return 0;}	
+		clean();	
+		}
+		else{
+		write_bram(tempAI,'O');	
+		}
+	}
+}*/
+
+
 int CPU::game_Play(){
 	int tempAI;
 	srand(time(0));
